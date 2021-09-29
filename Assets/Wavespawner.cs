d3da7fc2eb2f0102ae65df54284c094e70cf3b30
GaelMoney <25393@ma-web.nl>
2021-09-29 15:37:05 +0200

    test

---
+++ Assets/Wavespawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wavespawner : MonoBehaviour{
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countDown = 2f;
+
+    private int waveNumber = 1;
+
+
+    void Update()
+    {
+        if (countDown <= 0f)
+        {
+            SpawnWave();
+            countDown = timeBetweenWaves;
+        }
+
+        countDown  -= Time.deltaTime;
+    }
+
+    void SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+
+        waveNumber++;
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
